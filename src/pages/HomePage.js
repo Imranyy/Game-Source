b97cd99ad99ebfeb9ef451bb205cf626e1eb1051
imranyy <imranmatano6@gmail.com>
2022-12-05 18:01:15 +0300

    flowing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,32 +4,56 @@ const HomePage=()=>{
     //fetching all games from api
     const fetchGames=async()=>{
         try {
+            preloader();
             const apiKey='ddca4f81c1df4add8fda3feb725a7019';
             const url=`https://api.rawg.io/api/games?key=${apiKey}`;
             const response=await fetch(url,{
                 method:'GET'
             })
+            preloaderOff();
             const parseRes=await response.json();
-            console.log(parseRes.results);
-            setGames(parseRes.results)
+            console.log(parseRes);
+            setGames(parseRes.results);
         } catch (error) {
-            console.log(error.message)
+            preloaderOff();
+            console.log(error.message);
         }
     }
     useEffect(()=>{
         fetchGames();
     },[])
+     //preloader
+    const preloader=()=>{
+        const loader=document.querySelector('.preload');
+        loader.style.display='block';
+    }
+    const preloaderOff=()=>{
+        const loader=document.querySelector('.preload');
+        loader.style.display='none';
+    }
         return(
         <>
+            <div className='preload'></div>
             <div className="start home">
                 <div className='grid-home'>
                 {games?games.map((game)=>(
                     <div key={game.id} className="card">
                         <div className="card-image"><img src={game.background_image} alt='...'/></div>
                         <div className="card-content">
-                            <div className="card-header"><h2>Game One</h2></div>  
+                            <div className="card-header">
+                                <div className='platform'>
+                                {game.platforms.map(plat=>(
+                                    <div key={plat.id}>
+                                        {/* <img src={plat.platform.image_background} width='50'/> */}
+                                        <small>{plat.platform.name}</small>
+                                    </div>
+                                ))}
+                                </div>
+                                <h2>{game.name}</h2>
+                            </div>  
                             <div className="card-text">
                                 <p>{game.name}</p>
+                                <p>Rating: {game.rating}</p>
                             </div>  
                         </div>
                     </div>
